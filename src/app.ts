031871fcbfec7c54bb66dedcd74b42ef2177dc94
Soung jin Ryu <neocat@kakao.com>
2024-07-31 16:57:45 +0900

    Fix. api 코드 수정

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-import AutoLoad from '@fastify/autoload';
-import fastify from 'fastify';
+import { selectUser } from 'controllers/user';
+import { APIInteraction, InteractionResponseType, InteractionType } from 'discord-api-types/v10';
+import fastify, { FastifyReply } from 'fastify';
+import { createReply } from 'utils/discord';
 
-import { join } from 'path';
+import { verifyKey } from 'utils/interaction';
 
 //////////////////////////////////////////////////////////////////////
 // 환경변수
@@ -10,13 +12,92 @@ const server = fastify({
     logger: { transport: { target: '@fastify/one-line-logger' } },
 });
 
-server.get('/ping', { schema: { hide: true } }, async (req, res) => 'pong');
-
+server.get('/ping', async (req, res) => 'pong');
 // 플러그인
-server.register(AutoLoad, { dir: join(__dirname, 'plugins') });
+// server.register(AutoLoad, { dir: join(__dirname, 'plugins') });
 
 // 라우터
-server.register(AutoLoad, { dir: join(__dirname, 'routes'), ignorePattern: /.*(test|spec).*/ });
+server.post<{
+    Body: APIInteraction;
+    Params: {
+        id: string;
+    };
+}>(
+    '/bot/:id',
+    {
+        preHandler: [
+            (request, reply: FastifyReply, done: Function) =>
+                selectUser(request.params?.id)
+                    .then(user => {
+                        const { body, headers, rawBody } = request;
+                        if (
+                            verifyKey(
+                                rawBody || JSON.stringify(body),
+                                `${headers['x-signature-ed25519'] || headers['X-Signature-Ed25519']}`,
+                                `${headers['x-signature-timestamp'] || headers['X-Signature-Timestamp']}`,
+                                `${user.public_key}`
+                            )
+                        ) {
+                            return done();
+                        }
+                        throw new Error('User not found');
+                    })
+                    .catch(err => {
+                        reply.status(404).send({ error: 'User not found' });
+                    }),
+        ],
+        schema: {
+            // hide: true,
+            params: {
+                type: 'object',
+                properties: {
+                    id: { type: 'string', description: '봇 ID' },
+                },
+            },
+        },
+    },
+    (req, res) => {
+        const { body } = req;
+
+        // 상태체크 처리
+        if (body.type === InteractionType.Ping) {
+            return res.status(200).send({ type: InteractionResponseType.Pong });
+        }
+
+        const content = createReply(req, res);
+
+        const { app_permissions, application_id } = content;
+
+        // return done();        // 응답이 유동적인 처리를 해야함.
+        //     const interaction = fastify.interaction(req, res);
+        //     const msg: any = { ...body, ...body.data };
+        //     for (const [key, value] of interaction) {
+        //         msg[key as string] = value;
+        //     }
+
+        //     // isolated-vm
+
+        //     // (body.type, )
+
+        //     switch (body.type) {
+        //         case InteractionType.ApplicationCommand:
+        //             // app && app(msg);
+        //             break;
+        //         case InteractionType.MessageComponent:
+        //             // message && message(msg);
+        //             break;
+        //         case InteractionType.ModalSubmit:
+        //             // model && model(msg);
+        //             break;
+        //         case InteractionType.ApplicationCommandAutocomplete:
+        //             // autocomplete && autocomplete(msg);
+        //             break;
+        //         default:
+        //             break;
+        //     }
+        // }
+    }
+);
 
 export default server;
 
